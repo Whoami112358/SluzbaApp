03f6e893efb4dea76177cc963b101229d7422e82
przemko2912 <przemekdabrowski12@gmail.com>
2024-11-21 17:28:48 +0100

    Pierwsza kompilacja

---
+++ WebApplication2/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Zolnierz> Zolnierze { get; set; }
+        public DbSet<Harmonogram> Harmonogramy { get; set; }
+    }
+}
