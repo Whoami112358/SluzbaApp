6983152fc0771a72729c2c42a9e8eebdfb8509f1
przemko2912 <przemekdabrowski12@gmail.com>
2024-11-21 21:06:39 +0100

    Formularze dodawania dla admina: - pozycji harmonogramu - pozycji żołnierza

--- WebApplication2/Controllers/AdminController.cs
+++ WebApplication2/Controllers/AdminController.cs
@@ -65,5 +65,68 @@ namespace WebApplication2.Controllers
 
             return View(schedules);
         }
+
+        // GET: /Admin/AddSoldier
+        [HttpGet]
+        public async Task<IActionResult> AddSoldier()
+        {
+            // Pobierz listę pododdziałów, aby można było przypisać żołnierza do jednego z nich
+            var pododdzialy = await _context.Pododdzialy.ToListAsync();
+            ViewBag.Pododdzialy = pododdzialy;
+
+            return View();
+        }
+
+        // POST: /Admin/AddSoldier
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddSoldier(Zolnierz zolnierz)
+        {
+            if (ModelState.IsValid)
+            {
+                // Dodajemy nowego żołnierza do bazy
+                _context.Zolnierze.Add(zolnierz);
+                await _context.SaveChangesAsync();
+
+                // Po dodaniu żołnierza przekierowujemy na stronę z tabelą żołnierzy
+                return RedirectToAction("Soldiers");
+            }
+
+            // Jeśli model jest niepoprawny, ponownie wyświetlamy formularz z błędami
+            var pododdzialy = await _context.Pododdzialy.ToListAsync();
+            ViewBag.Pododdzialy = pododdzialy;
+
+            return View(zolnierz);
+        }
+        // GET: /Admin/AddSchedule
+        public IActionResult AddSchedule()
+        {
+            // Pobieramy dostępne pododdziały i rodzaje służb
+            var pododdzialy = _context.Pododdzialy.ToList();
+            ViewBag.Pododdzialy = pododdzialy;
+
+            return View();
+        }
+
+        // POST: /Admin/AddSchedule
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddSchedule(Harmonogram harmonogram)
+        {
+            if (ModelState.IsValid)
+            {
+                // Zapisujemy nowy harmonogram w bazie danych
+                _context.Harmonogramy.Add(harmonogram);
+                await _context.SaveChangesAsync();
+
+                // Po zapisaniu przekierowujemy do tabeli harmonogramów
+                return RedirectToAction("Schedule");
+            }
+
+            // Jeśli model jest niepoprawny, wyświetlamy formularz z błędami
+            var pododdzialy = _context.Pododdzialy.ToList();
+            ViewBag.Pododdzialy = pododdzialy;
+            return View(harmonogram);
+        }
     }
 }
